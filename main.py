7aac427110feaa6cf306f7623a1d4b7dc6a8d259
Kacper-So <66113631+Kacper-So@users.noreply.github.com>
2022-02-13 23:52:08 +0100

    main added

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import random
+import numpy as np
+import cv2
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+import pandas
+import os
+
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
